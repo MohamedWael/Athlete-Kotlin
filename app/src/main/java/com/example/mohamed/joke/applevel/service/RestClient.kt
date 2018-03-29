ee30b4353419f867ba291568134d03b2ce8ff60d
MohamedWael <mohamed.wael9371@gmail.com>
2018-03-29 14:32:52 +0200

    wrapping val

--- app/src/main/java/com/example/mohamed/joke/applevel/service/RestClient.kt
+++ app/src/main/java/com/example/mohamed/joke/applevel/service/RestClient.kt
@@ -1,10 +1,11 @@
 package com.example.mohamed.joke.applevel.service
 
+import com.example.mohamed.joke.applevel.AthleteConfig
 import com.example.mohamed.joke.module.home.pojo.AtheleticsResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface RestClient {
-    @GET("athletes.josn")
+    @GET(AthleteConfig.ATHELET_FILE)
     fun getAthelitics(): Call<AtheleticsResponse>
 }
